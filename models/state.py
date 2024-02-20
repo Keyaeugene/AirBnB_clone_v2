6c7f840fa3eb1520a159fbb210fa71996f2f9b0f
keyaeugene <keyaeugene@gmail.com>
2024-02-20 10:45:34 -0800

    correction

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Stte Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade='all, delete')
+
+    @property
+    def cities(self):
+        """Returns the list of City instances
+        with state_id equals to the current State.id"""
+        from models.city import City
+        from models import storage
+        listed = []
+        for v in storage.all(City).values():
+            if v.state_id == self.id:
+                listed.append(v)
+        return listed
