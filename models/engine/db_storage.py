e2e193968bb9672ef8ca832b525680981b739b54
keyaeugene <keyaeugene@gmail.com>
2024-02-20 10:04:58 -0800

    added a new test

---
+++ models/engine/db_storage.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+from os import environ
+from sqlalchemy import create_engine, text
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+class DBStorage():
+    """Placeholder"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """instantiation"""
+        user = environ.get('HBNB_MYSQL_USER')
+        pw = environ.get('HBNB_MYSQL_PWD')
+        ht = environ.get('HBNB_MYSQL_HOST')
+        db = environ.get('HBNB_MYSQL_DB')
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{pw}@{ht}/{db}',
+                                      pool_pre_ping=True)
+        if environ.get("HBNB_ENV") == "test":
+            Base.metadata.drop(self.__engine)
+
+    def all(self, cls=None):
+        """Returns a dict of the specified cls"""
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+            'State': State, 'City': City, 'User': User,
+            'Place': Place, 'Amenity': Amenity, 'Review':
+                Review
+            }
+
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+
+        ret = {}
+
+        if cls:
+            if type(cls) is not str:
+                cls = cls.__name__
+            for obj in self.__session.query(classes[cls]):
+                ret[str(cls) + '.' + obj.id] = obj
+        else:
+            for k in classes.keys():
+                for obj in self.__session.query(classes[k]):
+                    ret[str(k) + '.' + obj.id] = obj
+        return ret
+
+    def new(self, obj):
+        """add a new object to the data base"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all change"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete an object if not null"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reload all the objets"""
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        Base.metadata.create_all(self.__engine)
+
+        ses = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(ses)
+
+    def close(self):
+        """Closes the private session attribute"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            dict_filtered = {}
+            for k, v in self.__objects.items():
+                if isinstance(v, cls):
+                    dict_filtered[k] = v
+            return dict_filtered
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,17 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it's inside"""
+        if obj is not None:
+            to_delete = obj.__class__.__name__ + "." + obj.id
+            if to_delete in self.__objects:
+                del self.__objects[to_delete]
+
+    def close(self):
+        "Method for deserializing the JSON file to objects"
+        self.reload()
