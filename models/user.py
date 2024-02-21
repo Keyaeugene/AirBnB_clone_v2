13062b00cedd88cbdfc4b2f85a093781670e4f03
dennis <dennismutitu06@gmail.com>
2024-02-21 19:28:08 +0000

    Dbstorage User

--- models/user.py
+++ models/user.py
@@ -14,7 +14,26 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", backref="users",
-                          cascade="all, delete")
+                          cascade="all, delete-orphan")
 
     reviews = relationship("Review", backref="users",
-                           cascade="all, delete")
+                           cascade="all, delete-orphan")
+
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd
